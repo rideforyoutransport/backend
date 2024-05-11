cfbc4c7de621b0375ebe76c8108445eb28fd5a26
ankr2063 <ankr2063@gmil.com>
2024-05-11 13:12:24 +0530

    route fix

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,8 @@ const router = require('express').Router();
 
 
 router.use('/admin', require('./admin'));
-router.use('/admin/vendor', require('./admin/vendor'));
-router.use('/admin/booking', require('./admin/booking'));
-router.use('/admin/trips', require('./admin/trips'));
-router.use('/admin/stops', require('./admin/stops'));
-router.use('/admin/vehicle', require('./admin/vehicle'));
-router.use('/admin/driver', require('./admin/driver'));
-router.use('/admin/user', require('./admin/user'));
-//router.use('/admin/promocode', require('./admin/promocode'));
-
 router.use('/user', require('./user'));
-router.use('/user/booking', require('./user/booking'));
-router.use('/user/trips', require('./user/trips'));
-router.use('/user/stops', require('./user/stops'));
-
 router.use('/driver', require('./driver'));
-
 router.use('/vendor', require('./vendor'));
 
 
