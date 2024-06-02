f88d9b17425a53179b520de40a6b7c50726d7239
Sahil Yadav <sahilyadav@Sahils-MacBook-Air.local>
2024-06-03 02:45:14 +0530

    minor changes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'category_images')));
-app.use('/', ()=>{console.log("Server has connected");
+app.use('/', (req,res)=>{console.log("Server has connected");
 res.status(200).send({ message: "Connected succesfully" });});
 app.use('/api', routes);
 
