d62905b4d17e3a3ac7b33e0ca695268806052282
Ubuntu <ubuntu@ip-172-31-41-66.eu-north-1.compute.internal>
2024-06-09 04:24:10 +0000

    server start

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'category_images')));
-app.use('/', (req,res)=>{console.log("Server has connected");
-res.status(200).send({ message: "Connected succesfully" });});
+//app.use('/', (req,res)=>{console.log("Server has connected");
+//res.status(200).send({ message: "Connected succesfully" });});
 app.use('/api', routes);
 
 
---
+++ pocketbase/pb_migrations/1717731782_updated_trips.js
@@ -0,0 +1,16 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("0kctov44kvyeqny")
+
+  collection.createRule = ""
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("0kctov44kvyeqny")
+
+  collection.createRule = "vendor.verified=true"
+
+  return dao.saveCollection(collection)
+})
